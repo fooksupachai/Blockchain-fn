de066039847d1a4b3cc8117ae4ec3358f5f8c8cc
Supachai.Keemthong <fookfik1551@gmail.com>
2018-06-20 01:43:46 +0700

    Create fook Cryptocurrency and Build Blockchain for this currency

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data ;
         this.previousHash = previousHash ;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
-        
+        return  SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();   
     }
 }
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "19/06/2018", "Genesis block", "0");
+    }
+
+
+    getlastestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+    
+    addBlock(newBlock){
+        newBlock.previousHash = this.getlastestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let fookCoin  = new Blockchain();
+fookCoin.addBlock(new Block(1, "20/06/2018", { amount: 6}));
+fookCoin.addBlock(new Block(2, "21/06/2018", { amount: 10}));
+fookCoin.addBlock(new Block(3, "22/06/2018", { amount: 11}));
+
+console.log(JSON.stringify(fookCoin, null, 4));
